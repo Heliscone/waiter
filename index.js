c241f2a2072c6245f2d82f2b06293d713535b38c
Evan Lim <evanlim@Evans-MacBook-Pro.local>
2024-02-04 21:40:22 -0800

    loggypebble

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const port = process.env.PORT || 3000;
 app.get('/trackClick', (req, res) => {
     // Log the request details
     const data = `Clicked at: ${new Date()}, IP: ${req.ip}, User Agent: ${req.headers['user-agent']}\n`;
-    fs.appendFileSync('clicks.log', data);
-  
+    console.log('clicks.log', data);
     // Redirect to an actual image or URL
     res.redirect('https://www.google.com/');
   });  
